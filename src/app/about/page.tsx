45e018e4b27148726232e5947ae43107d96f4d55
poysa213 <hanaiayoucef@gmail.com>
2023-07-06 20:34:03 +0100

    update about content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,9 @@ import AnimatedText from "@/FramerMotion/AnimatedText";
 import AnimatedDiv from "@/FramerMotion/AnimatedDiv";
 import Image from "next/image";
 import { opacityVariant } from "@/content/FramerMotionVariants";
+import { TbBrandFiverr } from 'react-icons/tb'
 import PageTop from "@components/PageTop";
+import Link from "next/link";
 
 
 export default function AboutMe() {
@@ -43,43 +45,38 @@ export default function AboutMe() {
           <span className="font-bold">Youcef Hanaia</span>, also known as{" "}
           <span className="font-bold">poysa213.</span>
         </AnimatedText>
+        
         <AnimatedText
           className="text-slate-500 dark:text-slate-400 md:text-[1.2rem] font-mono p-4"
           variants={popUpFromBottomForText}
         >
-          I'm a passionate and self-taught{" "}
-          <span className="font-bold">Full Stack Developer</span> with a deep
-          interest in <span className="font-bold">Cybersecurity</span>.
-          Currently, I am pursuing my{" "}
-          <span className="font-bold">Master's</span> Degree in Computer
-          Science, further enriching my knowledge in the field.
+          I am a dedicated and accomplished <span className="font-bold">Full Stack Developer</span> with a strong focus on <span className="font-bold">Cybersecurity</span>. 
+          <br /> 
+          Currently, I am pursuing a <span className="font-bold">Master</span>'s Degree in  <span className="font-bold">Computer Science</span> to further enhance my expertise in this field.
+   
+        
         </AnimatedText>
 
         <AnimatedText
-          className="text-slate-500 dark:text-slate-400 font-medium font-mono md:text-[1.2rem] p-4"
+          className="text-slate-500 dark:text-slate-400 md:text-[1.2rem] font-mono p-4"
           variants={popUpFromBottomForText}
         >
-          My journey into <span className="font-bold">web development</span>{" "}
-          began during my high school days. Driven by a strong desire to learn,
-          I immersed myself in various resources such as tutorials, articles,
-          and hands-on projects. Along the way, I explored multiple programming
-          languages, including Python, Pascal (yes, even Pascal!), Brainfuck,
-          and more.
+         Currently, I work as a <span className="font-bold">Full Stack Developer</span> as a <span className="font-bold">Freelancer</span> on <span className="font-bold">Fiverr</span>, where I collaborate with clients from diverse backgrounds.
+         <br />
+          This experience has given me the opportunity to work on a wide range of projects, allowing me to enhance my <span className="font-bold">problem-solving</span> abilities and adapt to different requirements.
+          <br />
+          <br />
+          <div className="flex items-center">
+         <span className="font-bold">And here is a link to my account in Fiverr</span><Link target="_blank" className="ml-2 bg-[#1DBF73] p-1 rounded text-white" href={"https://www.fiverr.com/youcefpoysa"}><TbBrandFiverr /></Link>
+          </div>
+          <br />
+  
+        
         </AnimatedText>
-        <AnimatedText
-          className="text-slate-500 dark:text-slate-400 font-medium font-mono  md:text-[1.2rem] p-4"
-          variants={popUpFromBottomForText}
-        >
-          When I'm not coding, you'll find me engaging in exciting challenges on
-          platforms like <span className="font-bold">Hack The Box (HTB)</span>{" "}
-          and <span className="font-bold">PicoCTF</span>. I actively participate
-          in hacking machines, solving web and forensics challenges, and
-          continuously enhancing my skills in the realm of{" "}
-          <span className="font-bold">cybersecurity</span>. Additionally, I keep
-          myself up to date with the latest{" "}
-          <span className="font-bold">vulnerabilities,</span> particularly in
-          the web domain, by reading articles and exploring new attack vectors.
-        </AnimatedText>
+        
+        
+
+       
 
         <AnimatedText
           className="text-slate-500 dark:text-slate-400 font-medium font-mono p-3 text-[1.2rem] text-right animate-bounce "
